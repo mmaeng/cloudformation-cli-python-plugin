031651bc0825859a3d16b990ead26e59577c987c
Michael Maeng <mmaeng@amazon.com>
2022-11-01 23:34:34 -0400

    added modules nad hook to e2e test

--- e2e-test.sh
+++ e2e-test.sh
@@ -5,3 +5,22 @@ ls -la
 cfn init -t AWS::Foo::Bar -a RESOURCE $1 --use-docker
 ls -la
 mypy src/aws_foo_bar/ --strict --implicit-reexport
+cfn validate -vvv
+cfn generate -vvv
+cfn submit --dry-run -vvv
+DIR=$(mktemp -d)
+cd "$DIR"
+ls -la
+cfn init -t AWS::Foo::Bar::Module -a MODULE $1 --use-docker
+ls -la
+cfn validate -vvv
+cfn generate -vvv
+cfn submit --dry-run -vvv
+DIR=$(mktemp -d)
+cd "$DIR"
+ls -la
+cfn init -t AWS::Foo::Bar -a HOOK $1 --use-docker
+ls -la
+cfn validate -vvv
+cfn generate -vvv
+cfn submit --dry-run -vvv
